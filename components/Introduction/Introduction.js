2348a3550e195bb21bf15ab3dc0a9e47aee9741a
Jacob Eckert <jweckert17@gmail.com>
2020-09-30 14:20:38 -0500

    added background image to introduction section

---
+++ components/Introduction/Introduction.js
@@ -0,0 +1,10 @@
+import styles from './Introduction.module.css';
+
+const Introduction = () => {
+  return (
+    <section className={styles.introduction}>
+    </section>
+  );
+};
+
+export default Introduction;
