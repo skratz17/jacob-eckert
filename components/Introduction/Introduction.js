264c8ac8fdfbad916f68bc9e9dc2905ddd4364ec
Jacob Eckert <jweckert17@gmail.com>
2020-09-30 14:38:56 -0500

    making intro card

--- components/Introduction/Introduction.js
+++ components/Introduction/Introduction.js
@@ -3,6 +3,11 @@ import styles from './Introduction.module.css';
 const Introduction = () => {
   return (
     <section className={styles.introduction}>
+      <div className={styles.introCard}>
+        <h2>Full-Stack Web Developer</h2>
+        <h1>Jacob Eckert</h1>
+        <h2>In the Greater Nashville Area</h2>
+      </div>
     </section>
   );
 };
