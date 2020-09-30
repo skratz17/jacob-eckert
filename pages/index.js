9a566a636632027aced4959da7307fc9ecaf833c
Jacob Eckert <jweckert17@gmail.com>
2020-09-30 13:54:08 -0500

    added index page, setting up global styles

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Index = () => {
+  return (
+    <>
+      <Head>
+        <title>Jacob Eckert | Full-Stack Web Developer in Nashville, Tennessee</title>
+      </Head>
+      <div>test</div>
+    </>
+  );
+};
+
+export default Index;
