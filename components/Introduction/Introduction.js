264b8a339a0fa2eaeef6879376de422eb9a7036a
Jacob Eckert <jweckert17@gmail.com>
2020-09-30 15:29:29 -0500

    adding icons to introduction

--- components/Introduction/Introduction.js
+++ components/Introduction/Introduction.js
@@ -1,4 +1,23 @@
+import CircleIcon from '../CircleIcon/CircleIcon';
 import styles from './Introduction.module.css';
+
+const CONTACT_LINKS = [
+  { 
+    href: 'https://www.linkedin.com/in/jacob-w-eckert/', 
+    src: '/images/icons/linkedIn.png', 
+    alt: 'LinkedIn' 
+  },
+  { 
+    href: 'https://github.com/skratz17/',
+    src: '/images/icons/github.png', 
+    alt: 'GitHub' 
+  },
+  { 
+    href: 'mailto:jweckert17@gmail.com',
+    src: '/images/icons/email.png', 
+    alt: 'E-mail' 
+  }
+];
 
 const Introduction = () => {
   return (
@@ -7,6 +26,18 @@ const Introduction = () => {
         <h2>Full-Stack Web Developer</h2>
         <h1>Jacob Eckert</h1>
         <h2>In the Greater Nashville Area</h2>
+      </div>
+      <CircleIcon src="/images/introduction/jacobeckert.jpg"
+        alt="Jacob Eckert Headshot"
+        width="250" />
+      <div className={styles.contactIcons}>
+        {
+          CONTACT_LINKS.map(c => (
+            <a key={c.href} href={c.href} target="_blank" rel="noopener noreferrer">
+              <CircleIcon src={c.src} alt={c.alt} width="75" />
+            </a>
+          ))
+        }
       </div>
     </section>
   );
