88a5e021b58d4b47a2d9dde80fd76d4afd62d0d6
Jacob Eckert <jweckert17@gmail.com>
2020-10-05 09:39:03 -0500

    setting up smooth scroll navigation from table of contents

--- components/AboutMe/AboutMe.js
+++ components/AboutMe/AboutMe.js
@@ -3,7 +3,7 @@ import utilStyles from '../../styles/utilities.module.css';
 
 const AboutMe = () => {
   return (
-    <section className={styles.aboutMe + ' ' + utilStyles.section__dark} id="#about">
+    <section className={styles.aboutMe + ' ' + utilStyles.section__dark} id="about">
       <div className={styles.colLeft}>
         <h2 className={utilStyles.sectionHeading}>About Me</h2>
         <p className={styles.aboutMeText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore recusandae suscipit provident sed sint ipsam nam voluptates facere, eum accusantium tenetur. Expedita non officia iste. Vel debitis, natus amet distinctio dignissimos nisi sapiente sequi odit sunt similique fugit eaque molestias corrupti numquam modi veritatis harum qui atque dolore, blanditiis, eius obcaecati tenetur doloribus. Pariatur dolorem fuga tempora exercitationem vel tenetur.</p>
--- components/TableOfContents/TableOfContentsEntry/TableOfContentsEntry.js
+++ components/TableOfContents/TableOfContentsEntry/TableOfContentsEntry.js
@@ -1,12 +1,12 @@
 import styles from './TableOfContentsEntry.module.css';
 
 const TableOfContentsEntry = props => {
-  const { href, linkText } = props;
+  const { href, linkText, onClick } = props;
 
   return (
     <li className={styles.tableOfContentsEntry}>
       <div>
-        <a href={href}>{linkText}</a>
+        <a href={href} onClick={onClick}>{linkText}</a>
         <p>{props.children}</p>
       </div>
     </li>
