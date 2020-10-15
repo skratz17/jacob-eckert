c0e1c331a56126dfe626d000623b1cc45e4a8d71
Jacob Eckert <jweckert17@gmail.com>
2020-10-15 10:22:40 -0500

    adding more content to project detail and styling it

--- components/Projects/ProjectDetail/ProjectDetail.js
+++ components/Projects/ProjectDetail/ProjectDetail.js
@@ -1,4 +1,5 @@
 import ImageSlider from '../../ImageSlider/ImageSlider';
+import styles from './ProjectDetail.module.css';
 
 const ProjectDetail = props => {
   const { project } = props;
@@ -7,12 +8,20 @@ const ProjectDetail = props => {
     return null;
   }
 
-  const { name, images } = project;
+  const { name, images, description, features, deployedLink, projectLink } = project;
 
   return (
-    <div>
-      {name}
+    <div className={styles.projectDetail}>
       <ImageSlider images={images} />
+      <h3 className={styles.projectName}>{name}</h3>
+      <p className={styles.description}>{description}</p>
+      <ul className={styles.features}>
+        { features.map((f, index) => <li key={index}>{f}</li>) }
+      </ul>
+      <div className={styles.links}>
+        <a href={deployedLink} target="_blank" rel="noopener noreferrer">Check it out!</a>
+        <a href={projectLink} target="_blank" rel="noopener noreferrer">GitHub Repo</a>
+      </div>
     </div>
   );
 };
--- components/Projects/projectsConfig.js
+++ components/Projects/projectsConfig.js
@@ -23,7 +23,15 @@ export default [
         alt: 'Uhhh Wut Dashboard'
       }
     ],
-    shortDescription: 'A YouTube Transcription Hub for Language Learners'
+    shortDescription: 'A YouTube Transcription Hub for Language Learners',
+    description: 'uhhhwut is a single-page web application that crowdsources transcriptions of YouTube videos, and is targeted at learners of foreign languages who watch YouTube videos to immerse in their target language.',
+    features: [
+      'Single-page application built using modern, hook-based React.js.',
+      'Built with internationalization / I18N in mind - can be easily translated into any arbitrary language with the help of react-intl.',
+      'Responsively designed, so that the site can be easily used on any screen size.'
+    ],
+    deployedLink: 'https://uhhhwut.onrender.com/',
+    projectLink: 'https://github.com/skratz17/uhhhwut'
   },
   {
     name: 'Cynthia Cheatham Law',
