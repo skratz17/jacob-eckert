b198394615b8d56a61315c51bd23c6ca0b995f39
Jacob Eckert <jweckert17@gmail.com>
2020-10-15 10:05:06 -0500

    display none-ing prev/next button if inapplicable instead of re-rendering button set altogether

--- components/ImageSlider/ImageSlider.js
+++ components/ImageSlider/ImageSlider.js
@@ -8,25 +8,26 @@ const ImageSlider = props => {
   const [ activeImageIndex, setActiveImageIndex ] = useState(0);
 
   const renderControls = () => {
-    const controls = [];
-    
-    if(activeImageIndex !== 0) {
-      controls.push(
-        <button className={styles.button + ' ' + styles.buttonPrev} onClick={() => setActiveImageIndex(prevIndex => prevIndex - 1)}>
-          <i className="material-icons">navigate_before</i>
-        </button>
-      );
+    const prevClassNames = [ styles.button, styles.buttonPrev ];
+    const nextClassNames = [ styles.button, styles.buttonNext ];
+
+    if(activeImageIndex === 0) {
+      prevClassNames.push(styles.hidden)
     }
     
-    if(activeImageIndex !== images.length - 1) {
-      controls.push(
-        <button className={styles.button + ' ' + styles.buttonNext} onClick={() => setActiveImageIndex(prevIndex => prevIndex + 1)}>
-          <i className="material-icons">navigate_next</i>
-        </button>
-      );
+    if(activeImageIndex === images.length - 1) {
+      nextClassNames.push(styles.hidden);
     }
 
-    return controls;
+    return <>
+        <button className={prevClassNames.join(' ')} onClick={() => setActiveImageIndex(prevIndex => prevIndex - 1)}>
+          <i className="material-icons">navigate_before</i>
+        </button>
+
+        <button className={nextClassNames.join(' ')} onClick={() => setActiveImageIndex(prevIndex => prevIndex + 1)}>
+          <i className="material-icons">navigate_next</i>
+        </button>
+    </>;
   };
 
   return (
